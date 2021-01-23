a813ff9791a8e3533827b28101aaf5c64d34ab1e
Bagus setiawan <setiawanbagus4315@gmail.com>
2021-01-23 09:52:16 +0700

    add files !

---
+++ PokemonCard/PokemonCard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonCard
+//
+//  Created by Bagus setiawan on 23/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
